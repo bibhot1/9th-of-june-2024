a7979f2617fef00a258f7de608dcf050a11fc619
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-11 06:42:21 +1200

    Add project files.

---
+++ 9th of june 2024/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("give me a number");
+string input = Console.ReadLine();
+int number1 = 0;
+bool isNumber = int.TryParse(input, out number1);
+if (isNumber)
+{
+    Console.WriteLine("good job");
+}
+else
+{
+    Console.WriteLine("Do better");
+}
+
+number1++;
+Console.WriteLine("user has inputed +1 " + number1);
